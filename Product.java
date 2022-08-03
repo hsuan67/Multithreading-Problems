8d80d34054462d4cd63ce36a5cb77cf8f62ab6f8
hsuan <86561823+hsuan67@users.noreply.github.com>
2022-08-04 02:35:55 +0800

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,22 @@
+package consumer_producer_problem;
+
+public class Product implements Runnable {
+
+	private String name;
+
+	public Product(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+	}
+}
